04f10c5c1eea85c9896318ab5f1a55f4bf566098
jhnemogap <jhnemogap@gmail.com>
2022-01-23 13:05:35 -0500

    10 create the global container (#11)
    * feat: created base struct of the main layout
    
    * feat: added styles to container div as GlobalMainLayout
    
    * feat(styles): added styles and components to change theme interactives
    
    Por cuestiones entre importacion de archivo svg como un next/image y
    la necesidad de cambiar el color se crean componente que contienen
    el codigo html del svg
    
    * refactor(styles): toggleTheme is now a usehook and set correct data-theme when start
    
    * feat: start with the theme makes match with prefers-color-scheme

---
+++ src/components/layouts/GlobalMainLayout/GlobalMainLayout.tsx
@@ -0,0 +1,31 @@
+import { MoonIcon, SunIcon } from "components";
+
+import { useToggleTheme } from "hooks";
+
+import { LIGHT_THEME_NAME } from "constants/general";
+import styles from "./GlobalMainLayout.module.scss";
+
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export function GlobalMainLayout(props: Props): JSX.Element {
+  const { children }: Props = props;
+
+  const [themeState, setToggleTheme] = useToggleTheme();
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 aria-label={"logo of devfinder"}>devfinder</h1>
+        <button onClick={setToggleTheme} className={styles.toggleTheme}>
+          <h4 className={styles.text}>{themeState.next}</h4>
+          {themeState.active === LIGHT_THEME_NAME ? <MoonIcon /> : <SunIcon />}
+        </button>
+      </header>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const HomePage: NextPage = () => {
   const router: NextRouter = useRouter();
 
   useEffect((): void => {
-    router.replace(routeUsername()).then();
+    router.replace(routeUsername({})).then();
   }, []);
 
   return <div>Homepage</div>;
