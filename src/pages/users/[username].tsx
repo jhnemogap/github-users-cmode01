f4b55019f59b37a64dec037d7eb6a6753780fba1
jhnemogap <jhnemogap@gmail.com>
2022-01-24 00:11:44 -0500

    (issue #13) create username search
    * feat: new component SearchBar, basic struct
    
    * feat(styles): styles with interactive selections to SearchBar's elements (input and btn)

--- src/pages/users/[username].tsx
+++ src/pages/users/[username].tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 
+import { SearchBar } from "components";
+
 import type { NextPage } from "next";
 import type { NextRouter } from "next/router";
 
@@ -7,22 +9,29 @@ const UsernamePage: NextPage = () => {
   const router: NextRouter = useRouter();
   const { username } = router.query;
 
-  return (
-    <div>
-      <h1>{username}</h1>
-      <h2>Lorem ipsum dolor sit amet</h2>
-      <h3>@{username}</h3>
-      <h4>repos</h4>
+  const handleOnSearch = (value: string) => console.info(value);
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet architecto consequuntur, corporis ducimus
-        eum, excepturi iste itaque maiores nam neque nihil, quae quasi sit totam. Assumenda fugit nemo vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur earum fuga neque nostrum
-        ratione rerum sapiente sit vero. Alias culpa dolorum eius eligendi eum omnis perspiciatis vitae voluptates?
-      </p>
-    </div>
+  return (
+    <section style={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}>
+      <SearchBar onSearch={handleOnSearch} />
+
+      <article>
+        <h1>{username}</h1>
+        <h2>Lorem ipsum dolor sit amet</h2>
+        <h3>@{username}</h3>
+        <h4>repos</h4>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet architecto consequuntur, corporis ducimus
+          eum, excepturi iste itaque maiores nam neque nihil, quae quasi sit totam. Assumenda fugit nemo vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur earum fuga neque
+          nostrum ratione rerum sapiente sit vero. Alias culpa dolorum eius eligendi eum omnis perspiciatis vitae
+          voluptates?
+        </p>
+      </article>
+    </section>
   );
 };
 
