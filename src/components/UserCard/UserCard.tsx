1465a8f9b3c6d592aaf3823f58b861f349c6338b
jhnemogap <jhnemogap@gmail.com>
2022-01-25 04:56:45 -0500

    feat(styles): user card Footer has all final designs for different width devices

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
+import { CompanyIcon, LocationIcon, TwitterIcon, WebsiteIcon } from "components";
+
 import { TEXT_BIOGRAPHY_DEFAULT, TEXT_NOT_AVAILABLE } from "constants/general";
 import styles from "./UserCard.module.scss";
 
-interface Props {
+export interface Props {
   username: string;
   avatar: string;
   joined: string; // is a Date on UTC
@@ -38,6 +42,7 @@ export function UserCard(props: Props): JSX.Element {
   return (
     <article className={styles.card}>
       <header className={styles.header}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img alt={"avatar"} src={avatar} width="100%" className={styles.avatar} />
 
         <section className={styles.intro}>
@@ -52,7 +57,7 @@ export function UserCard(props: Props): JSX.Element {
           </p>
         </section>
 
-        <section className={styles.bio} style={{ opacity: !biography ? "50%" : "" }}>
+        <section className={styles.bio} style={setCssOpacityOnStyle(!biography, "75%")}>
           <p aria-roledescription="user biography">{biography || TEXT_BIOGRAPHY_DEFAULT}</p>
         </section>
       </header>
@@ -61,27 +66,64 @@ export function UserCard(props: Props): JSX.Element {
         <section className={styles.statisticsSection}>
           <div className={styles.statisticItem}>
             <h4>repos</h4>
-            <p className={"h2"}>{repos}</p>
+            <p className={"h2 textColorSecondary"}>{repos}</p>
           </div>
 
           <div className={styles.statisticItem}>
             <h4>followers</h4>
-            <p className={"h2"}>{followers}</p>
+            <p className={"h2 textColorSecondary"}>{followers}</p>
           </div>
 
           <div className={styles.statisticItem}>
             <h4>following</h4>
-            <p className={"h2"}>{following}</p>
+            <p className={"h2 textColorSecondary"}>{following}</p>
           </div>
         </section>
       </main>
 
       <footer className={styles.footer}>
-        <p>{location || TEXT_NOT_AVAILABLE}</p>
-        <p>{twitter || TEXT_NOT_AVAILABLE}</p>
-        <p>{website || TEXT_NOT_AVAILABLE}</p>
-        <p>{company || TEXT_NOT_AVAILABLE}</p>
+        <div className={styles.detail} style={setCssOpacityOnStyle(!location, "50%")}>
+          <span className={styles.icon}>
+            <LocationIcon />
+          </span>
+          <p>{location || TEXT_NOT_AVAILABLE}</p>
+        </div>
+        <div className={styles.detail} style={setCssOpacityOnStyle(!twitter, "50%")}>
+          <span className={styles.icon}>
+            <TwitterIcon />
+          </span>
+          <p>{twitter || TEXT_NOT_AVAILABLE}</p>
+        </div>
+        <div className={styles.detail} style={setCssOpacityOnStyle(!website, "50%")}>
+          <span className={styles.icon}>
+            <WebsiteIcon />
+          </span>
+          <p>
+            {!website && TEXT_NOT_AVAILABLE}
+            {!!website && (
+              <Link href={website} passHref>
+                <a target="_blank" rel="noreferrer">
+                  {website}
+                </a>
+              </Link>
+            )}
+          </p>
+        </div>
+        <div className={styles.detail} style={setCssOpacityOnStyle(!company, "50%")}>
+          <span className={styles.icon}>
+            <CompanyIcon />
+          </span>
+          <p>{company || TEXT_NOT_AVAILABLE}</p>
+        </div>
       </footer>
     </article>
   );
+}
+
+function setCssOpacityOnStyle(
+  expression: boolean = false,
+  percentTrue: string = "",
+  percentFalse: string = "",
+): { opacity: string } {
+  return { opacity: expression ? percentTrue : percentFalse };
 }
